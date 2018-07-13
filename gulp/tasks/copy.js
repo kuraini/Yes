dec7eef9ac49f4878746141c037f283287674da8
Svetlana Borisova <kuraini@yandex.ru>
2018-07-13 20:07:29 +0300

    Добавляет стили для планшетной версии

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,7 +2,8 @@ module.exports = function () {
   $.gulp.task('copy', function () {
     return $.gulp.src([
         'src/fonts/**/*.{woff,woff2}',
-        'src/*.html'
+        'src/*.html',
+        'src/js/libs/*js'
       ], {
         base: 'src'
       })
--- gulp/tasks/webp.js
+++ gulp/tasks/webp.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   $.gulp.task('webp', function () {
-    return $.gulp.src('src/img/content/*.{png,jpg}')
+    return $.gulp.src('src/img/content/**/*.{png,jpg}')
       .pipe($.webp({quality: 90}))
       .pipe($.gulp.dest('build/img/content'))
   });
