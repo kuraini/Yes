6c27d80ab7f458d3d03217a9b16e601b0c81c874
Svetlana Borisova <kuraini@yandex.ru>
2018-07-17 15:11:29 +0300

    Добавляет минификацию скриптов

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,6 +2,9 @@ module.exports = function () {
   $.gulp.task('scripts', function () {
     return $.gulp.src('src/js/main.js')
       .pipe($.gulp.dest('build/js/'))
+      .pipe($.uglify())
+      .pipe($.rename('main.min.js'))
+      .pipe($.gulp.dest('build/js/'))
       .pipe($.server.reload({
         stream: true
       }));
